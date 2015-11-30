0996059dbd05cfdc7b536c51eb2e1947ce991cef
Ryan Ramage <ryan.ramage@gmail.com>
2015-11-30 14:55:34 -0700

    renable tests

--- test/unit.js
+++ test/unit.js
@@ -17,23 +17,23 @@ var loginDetails = {
   description: ''
 }
 
-// test('test basic sequence: adding a fingerprint, login, and search', function (t) {
-//   setup('test1', t, function (err, db) {
-//     if (err) return console.log(err)
-//     var whoaru = Whoaru(db)
-//     whoaru.addFingerprint(fingerprint, function (err) {
-//       t.error(err)
-//       whoaru.login(fingerprint, userloginID, loginType, app, space, loginDetails, function (err, person) {
-//         t.error(err)
-//         whoaru.person(fingerprint, app, space, function (err, person2) {
-//           t.error(err)
-//           t.equals(person._id, person2._id)
-//           t.end()
-//         })
-//       })
-//     })
-//   })
-// })
+test('test basic sequence: adding a fingerprint, login, and search', function (t) {
+  setup('test1', t, function (err, db) {
+    if (err) return console.log(err)
+    var whoaru = Whoaru(db)
+    whoaru.addFingerprint(fingerprint, function (err) {
+      t.error(err)
+      whoaru.login(fingerprint, userloginID, loginType, app, space, loginDetails, function (err, person) {
+        t.error(err)
+        whoaru.person(fingerprint, app, space, function (err, person2) {
+          t.error(err)
+          t.equals(person._id, person2._id)
+          t.end()
+        })
+      })
+    })
+  })
+})
 
 test('test same fingerprint used in two spaces on the same app', function (t) {
   var space2 = 'rwp-2037'
